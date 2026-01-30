7a7fbd6fb1fa0e1a4cfcacede8c58af7d5a1ab0e
evgen <evgeniya.tyutyunik@gmail.com>
2026-01-30 18:10:26 +0200

    add urls to accounts, catalog and debug in task-manager-py-django app

--- task_manager_py_django/urls.py
+++ task_manager_py_django/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+import debug_toolbar
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("catalog/", include("catalog.urls", namespace="catalog")),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
